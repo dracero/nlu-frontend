3a4784c28d612deca00e12939d6faff0faf50a98
Manuel <75450615+vlk-yr@users.noreply.github.com>
2021-11-16 19:07:23 -0300

    Update EliminarNLU.js

--- src/EliminarNLU.js
+++ src/EliminarNLU.js
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 import FormID from "./FormID";
+import Alert from "@mui/material/Alert";
 import axios from "axios";
+import url from "./index.js";
+import "./styles.css";
 
-import './styles.css'
-
+let errorMessage = '';
 
 const EliminarNLU = () => {
+
   const [deleteNluId, setDeleteNluId] = useState("");
+  const [state, setState] = useState('');
 
   const deleteNLU = (event) => {
+    
     event.preventDefault()
   
-    axios.delete("http://localhost:3000/nlu_structure/" + deleteNluId)
-        .then(returnedNLU => {
-          console.log("Se eliminó con exito el nlu: " + returnedNLU.data.name);
-          setDeleteNluId("")
-        })
-        .catch(error => {
-          console.log(error);
-        })
+    axios
+      .delete(url + "nlu_structure/" + deleteNluId)
+      .then(returnedNLU => {
+        //console.log("Se eliminó con éxito el nlu: " + returnedNLU.data.name);
+        setState('Success');
+        setDeleteNluId("");
+      })
+      .catch(error => {
+        errorMessage = error.response.data.name;
+        setState('Error');
+        console.log(error);
+      })
   }
 
   const handleNluChangeID = (event) => {
@@ -26,10 +35,27 @@ const EliminarNLU = () => {
   }
 
   return (
+    
     <div>
       <h1>Eliminar NLU</h1>
       <FormID onSubmit={deleteNLU} 
               handleNluChangeID={handleNluChangeID} />
+      
+      {(state === 'Success') &&
+        <div>
+          <Alert variant="outlined" severity="success">
+            NLU eliminado exitosamente.
+          </Alert>
+        </div>
+      }
+
+      {(state === 'Error') &&
+        <div>
+          <Alert variant="outlined" severity="error">
+            Error: ID no encontrado.
+          </Alert>
+        </div>
+      }
     </div>
   );
 };
